860fb960a039db66a0636082faffc6999f4b1397
olga <lelia363@yandex.ru>
2020-08-12 13:32:26 +0300

    test feature

--- app/app/test.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from app.calc import subtract
-
-
-class CalcTests(TestCase):
-
-    def test_subtract_number(self):
-        self.assertEqual(subtract(12, 6), 6)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,3 +10,13 @@ class ModelTests(TestCase):
 
     def test_subtract_numbers(self):
         self.assertEqual(subtract(10, 5), 5)
+
+    def test_create_user_with_successful(self):
+        email = 'test@urkrainedev.com'
+        password = 'testpass'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
