fdddf0d725734d4b95e472ad5a14322a08abeda1
olga <lelia363@yandex.ru>
2020-08-12 13:33:47 +0300

    new test pass

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,7 +13,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_successful(self):
         email = 'test@urkrainedev.com'
-        password = 'testpass'
+        password = 'newtestpass'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
